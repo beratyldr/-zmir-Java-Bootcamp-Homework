049ee62419e53b8f9431e61d77cbc84fc1da98b0
beratyldr <beratyldr.21@gmail.com>
2021-10-02 11:15:41 +0300

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,16 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	        // write your code here
+    public static void main(String[] args) throws IOException {
 
 
     }
